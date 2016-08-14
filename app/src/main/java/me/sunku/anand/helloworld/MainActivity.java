31233b93c29e8e85fb87e3377881cde5909e23c7
Anand Sunku <anand.sunku@gmail.com>
2016-08-14 20:34:11 +0530

    worked on my phone

--- app/src/main/java/me/sunku/anand/helloworld/MainActivity.java
+++ app/src/main/java/me/sunku/anand/helloworld/MainActivity.java
@@ -1,8 +1,12 @@
 package me.sunku.anand.helloworld;
 import android.app.Activity;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by anand on 06/08/16.
@@ -11,17 +15,47 @@ import android.os.Bundle;
 /* added to github with the below link
 * https://www.londonappdeveloper.com/how-to-use-git-hub-with-android-studio/
 * */
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements View.OnClickListener{
 
     SQLiteDatabase db;
+    Button btnViewAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnViewAll = (Button)findViewById(R.id.btnViewAll);
+        btnViewAll.setOnClickListener(this);
+
         db = openOrCreateDatabase("studentsDB", Context.MODE_PRIVATE,null);
         db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR);");
         db.execSQL("INSERT INTO student VALUES('simple');");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnViewAll){
+            Cursor c= db.rawQuery("SELECT * FROM student",null);
+            if(c.getCount() == 0)
+            {
+                showMessage("Error","No records found");
+                return;
+            }
+            StringBuffer buffer = new StringBuffer();
+            while(c.moveToNext())
+            {
+                buffer.append("Rollno: "+ c.getString(0)+"\n");
+            }
+            showMessage("StudentDetails", buffer.toString());
+        }
+    }
+
+    public void showMessage(String title, String message) {
+        Builder builder = new Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
---
+++ app/src/main/java/me/sunku/anand/helloworld/TodoListSQLHelper.java
@@ -0,0 +1,35 @@
+package me.sunku.anand.helloworld;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by anand on 13/08/16.
+ */
+
+public class TodoListSQLHelper extends SQLiteOpenHelper {
+    public static final String DB_NAME = "me.sunku.anand.androidtodo";
+    public static final String TABLE_NAME = "TODO_LIST";
+    public static final String COL1_TASK = "todo";
+    public static final String _ID = BaseColumns._ID;
+
+    public TodoListSQLHelper(Context context){
+        super(context, DB_NAME, null,1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTodoListTable = "CREATE TABLE "+ TABLE_NAME +" ( _id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    COL1_TASK + " TEXT)";
+        db.execSQL(createTodoListTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+    }
+}
