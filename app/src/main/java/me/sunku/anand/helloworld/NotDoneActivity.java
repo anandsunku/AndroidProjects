761f7081b88e554f57122d9551794fbac95de9c8
Anand Sunku <anand.sunku@gmail.com>
2016-08-15 15:48:00 +0530

    worked on phone after debugging, but initially crashed for some reason

--- app/src/main/java/me/sunku/anand/helloworld/NotDoneActivity.java
+++ app/src/main/java/me/sunku/anand/helloworld/NotDoneActivity.java
@@ -1,7 +1,9 @@
 package me.sunku.anand.helloworld;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -13,20 +15,43 @@ import me.sunku.anand.helloworld.R;
  */
 
 public class NotDoneActivity extends Activity {
+    SQLiteDatabase db;
+    String taskid,taskname, reasonTitle, reasonDesc;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nodetails);
 
         Bundle extras = getIntent().getExtras();
-        String value1 = extras.getString("id");
-        String value2 = extras.getString("taskName");
-        value1 = value1 + " -> " + value2;
+        taskid = extras.getString("id");
+        taskname = extras.getString("taskName");
+        String title;
+        title = taskid + " -> " + taskname;
         TextView tv = (TextView)findViewById(R.id.habitName);
-        tv.setText(value1);
+        tv.setText(title);
+
+        db = openOrCreateDatabase("me.sunku.anand.androidtodo", Context.MODE_PRIVATE,null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS NotDoneActivity(habitid VARCHAR, " +
+                "habitname VARCHAR, ReasonTitle VARCHAR, ReasonDesc VARCHAR, TimeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP );");
     }
 
     public void onUpdateButtonClick(View view) {
+
+        //collect all the details
+        //editReason, editNotReasonDetailText
+        reasonTitle = ((TextView)findViewById(R.id.editReason)).getText().toString();
+        reasonDesc = ((TextView)findViewById(R.id.editNotReasonDetailText)).getText().toString();
+
+        String insertQueryStr;
+
+        insertQueryStr = "INSERT INTO NotDoneActivity VALUES('"+taskid+"',"+
+                "'"+taskname+"'," +
+                "'"+reasonTitle+"'," +
+                "'"+reasonDesc+"'," +
+                "DATETIME('now'));";
+
+        db.execSQL(insertQueryStr);
+
         Intent i = new Intent();
         setResult(RESULT_OK,i);
         finish();
